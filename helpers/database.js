cb7261c6b907fbb5826e813cfd75071ac5c22033
AutiCodes <kelvin@aerobytes.nl>
2025-01-25 23:16:18 +0100

    Working DB

---
+++ helpers/database.js
@@ -0,0 +1,67 @@
+// helpers/database.js
+
+import {Logging} from './logging.js';
+import { config } from 'dotenv';
+config();
+
+import mysql from 'mysql2';
+
+class Database {
+    static connection;
+
+    static init() {
+        if (!Database.connection) {
+            Database.connection = mysql.createConnection({
+                host: process.env.DATABASE_HOST,
+                user: process.env.DATABASE_USER,
+                password: process.env.DATABASE_PASSWORD,
+                database: process.env.DATABASE_NAME,
+                port: process.env.DATABASE_NAME,
+            })
+        }
+    }
+
+    static connect() {
+        if (!Database.connection) {
+            Database.init();
+        }
+
+        Database.connection.connect((err) => {
+            if (!err) {
+                Logging.debug('Connected to database');
+                return;
+            }
+
+            Logging.error(`Database connection error inside helpers/database.js: ${err}`);
+        })
+    }
+
+    static query(sql, params = []) {
+        return new Promise((resolve, reject) => {
+            if (!Database.connection) {
+                Database.init();
+            }
+
+            Database.connection.query(sql, params, (err, result) => {
+                if (!err) {
+                    resolve(result);
+                }
+                reject(err);
+            })
+        })
+    }
+
+    static close() {
+        if (Database.connection) {
+            Database.connection.end((err) => {
+                if (!err) {
+                    Logging.debug('Database connection closed');
+                    return;
+                }
+                Logging.error('Error closing the connection: ', err)
+            });
+        }
+    }
+}
+
+export default Database;
--- index.js
+++ index.js
@@ -2,12 +2,25 @@ import { config } from 'dotenv';
 config();
 
 import { Client, Events, GatewayIntentBits } from 'discord.js';
-import { Logging } from './helpers/logging.js';
+import {Logging} from './helpers/logging.js';
+import Database from './helpers/database.js';
+
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on(Events.ClientReady, readyClient => {
     Logging.info(`Logged in as ${client.user.tag}!`);
+
+    // Testing
+    Database.connect();
+    Database.query('SELECT * FROM Users')
+        .then(results => {
+            Logging.info(JSON.stringify(results));
+        })
+        .catch(err => {
+            Logging.error(JSON.stringify(err.message));
+        });
+        Database.close();
 })
 
 client.login(process.env.DISCORD_TOKEN);
