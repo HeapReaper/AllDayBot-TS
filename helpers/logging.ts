18b22104541260223e311f07f04f6b871473d8b2
AutiCodes <kelvin@aerobytes.nl>
2025-01-29 14:35:41 +0100

    Fixed stuff

--- helpers/logging.ts
+++ helpers/logging.ts
@@ -4,19 +4,19 @@ import { config } from 'dotenv';
 config();
 
 export class Logging {
-    static info(message) {
+    static info(message: string): void {
         console.log(`INFO: ${message}`);
     }
 
-    static warn(message) {
+    static warn(message: string): void {
         console.warn(`WARNING: ${message}`);
     }
 
-    static error(message) {
+    static error(message: string): void {
         console.error(`ERROR: ${message}`);
     }
 
-    static debug(message) {
+    static debug(message: string): void {
         if (process.env.ENVIRONMENT !== 'debug') {
             return;
         }
