10beb16b93be2aebeba19896b1b74645b1255b5c
AeroBytes <kelvin@aerobytes.nl>
2025-02-11 15:49:35 +0100

    Added comments to env.ts helper

--- helpers/env.ts
+++ helpers/env.ts
@@ -4,6 +4,13 @@ import { config } from 'dotenv';
 import * as process from 'node:process';
 config();
 
+/**
+ * Gets an environment value from .env
+ *
+ * @param key - The environment key. Example: DISCORD_TOKEN.
+ * @param fallback - Fallback value. Defaults to an empty string.
+ * @return string|undefined
+ */
 export const getEnv = (key: string, fallback: string = ''): string|undefined => {
     const envValue: string|undefined = process.env[key];
 
