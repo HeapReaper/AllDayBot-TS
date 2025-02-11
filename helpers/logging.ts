edf4dfdefc157d4212ae1afbc8048ddc8815f092
AeroBytes <kelvin@aerobytes.nl>
2025-02-11 13:44:41 +0100

    Added logging comments

--- helpers/logging.ts
+++ helpers/logging.ts
@@ -4,18 +4,43 @@ import { config } from 'dotenv';
 config();
 
 export class Logging {
+    /**
+     * Logs info messages to the terminal.
+     *
+     * @param message - The message that needs to be logged.
+     * @returns void - Returns nothing.
+     */
     static info(message: string): void {
         console.log(`INFO: ${message}`);
     }
 
+    /**
+     * Logs warning messages to the terminal.
+     *
+     * @param message - The message that needs to be logged.
+     * @returns void - Returns nothing.
+     */
     static warn(message: string): void {
         console.warn(`WARNING: ${message}`);
     }
 
+    /**
+     * Logs error messages to the terminal.
+     *
+     * @param message - The message that needs to be logged.
+     * @returns void - Returns nothing.
+     */
     static error(message: string): void {
         console.error(`ERROR: ${message}`);
     }
 
+    /**
+     * Logs debug messages to the terminal.
+     * It display the error message only if environment is set to 'debug'.
+     *
+     * @param message - The message that needs to be logged.
+     * @returns void - Returns nothing.
+     */
     static debug(message: string): void {
         if (process.env.ENVIRONMENT !== 'debug') {
             return;
