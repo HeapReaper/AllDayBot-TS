7aa36fac7d694276672be1fd2b5037fda9736ae0
AeroBytes <kelvin@aerobytes.nl>
2025-02-11 16:01:15 +0100

    Improved JSdocs comments in database.ts and env.ts

--- helpers/env.ts
+++ helpers/env.ts
@@ -7,9 +7,9 @@ config();
 /**
  * Gets an environment value from .env
  *
- * @param key - The environment key. Example: DISCORD_TOKEN.
- * @param fallback - Fallback value. Defaults to an empty string.
- * @return string|undefined
+ * @param {string} key - The environment key. Example: DISCORD_TOKEN.
+ * @param {string} fallback - Fallback value. Defaults to an empty string.
+ * @return {string|undefined} - Returns a string or undefined.
  */
 export const getEnv = (key: string, fallback: string = ''): string|undefined => {
     const envValue: string|undefined = process.env[key];
