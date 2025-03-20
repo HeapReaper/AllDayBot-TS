82d7223f8191b977fb54ef36e4180e4fffede329
AeroBytes <kelvin@aerobytes.nl>
2025-03-20 21:47:38 +0100

    Improved logging reading visibility (and added colors! 😎

--- helpers/logging.ts
+++ helpers/logging.ts
@@ -1,9 +1,10 @@
 // helpers/logging.js
 
 import { getEnv } from '@helpers/env.ts';
+import chalk from 'chalk';
 
 /**
- * A helper class for logging messages to the console.
+ * Logging messages to the console.
  *
  * @class Logging
  */
@@ -11,43 +12,51 @@ export class Logging {
     /**
      * Logs info messages to the terminal.
      *
-     * @param {string} message - The message that needs to be logged.
+     * @param {string|number} message - The message that needs to be logged.
      * @returns void - Returns nothing.
      */
-    static info(message: string): void {
-        console.log(`INFO: ${message}`);
+    static info(message: string|number): void {
+        const now = new Date();
+        //console.log(`${chalk.green('INFO')} - ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()} - ${message}`);
+        console.log(`[${now.getDate()}-${now.getMonth()}-${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getUTCSeconds()}] [${chalk.green('INFO')}]  ${message}`);
     }
 
     /**
      * Logs warning messages to the terminal.
      *
-     * @param {string} message - The message that needs to be logged.
+     * @param {string|number} message - The message that needs to be logged.
      * @returns void - Returns nothing.
      */
-    static warn(message: string): void {
-        console.warn(`WARNING: ${message}`);
+    static warn(message: string|number): void {
+        const now = new Date();
+        
+        console.log(`[${now.getDate()}-${now.getMonth()}-${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getUTCSeconds()}] [${chalk.yellow('WARN')}]  ${message}`);
     }
 
     /**
      * Logs error messages to the terminal.
      *
-     * @param {string} message - The message that needs to be logged.
+     * @param {string|number} message - The message that needs to be logged.
      * @returns void - Returns nothing.
      */
-    static error(message: string): void {
-        console.error(`ERROR: ${message}`);
+    static error(message: string|number): void {
+        const now = new Date();
+        
+        console.log(`[${now.getDate()}-${now.getMonth()}-${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getUTCSeconds()}] [${chalk.red('ERROR')}] ${message}`);
     }
 
     /**
      * Logs debug messages to the terminal.
      * It display the error message only if environment is set to 'debug'.
      *
-     * @param {string} message - The message that needs to be logged.
+     * @param {string|number} message - The message that needs to be logged.
      * @returns void - Returns nothing.
      */
-    static debug(message: string): void {
+    static debug(message: string|number): void {
+        const now = new Date();
+        
         if (getEnv('ENVIRONMENT') !== 'debug') return;
-
-        console.debug(`DEBUG: ${message}`);
+        
+        console.log(`[${now.getDate()}-${now.getMonth()}-${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}:${now.getUTCSeconds()}] [${chalk.blue('DEBUG')}] ${message}`);
     }
 }
