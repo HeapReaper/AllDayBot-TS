389f4a94b03d9a58569ed2993cf201016bc798da
AeroBytes <kelvin@aerobytes.nl>
2025-03-20 20:34:03 +0100

    Improved birthday tasks

--- modules/Birthday/tasks.ts
+++ modules/Birthday/tasks.ts
@@ -22,26 +22,29 @@ export default class Tasks {
 			
 			const now = new Date();
 			
-			if (now.getHours() == 19 && now.getMinutes() == 31) {
+			if (now.getHours() == 10 && now.getMinutes() == 0o0) {
 				// To prevent spamming
 				this.counter = 1;
 				
 				const birthdays: any[] = await Database.select('birthday');
 				
-				birthdays.forEach(birthday => {
+				for (const birthday of birthdays) {
 					const paredBirthday = new Date(Date.parse(birthday.birthdate));
 					if ((paredBirthday.getMonth() + 1) !== (now.getMonth() + 1) && paredBirthday.getDate() !== now.getDate()) {
-						// @ts-ignore
 						continue;
 					}
 					
 					const channel = this.client.channels.cache.get(getEnv('GENERAL')!);
-					if (!channel) return;
+					if (!channel) {
+						Logging.error('General channel not found in birthday tasks.');
+						return;
+					}
 					
-				});
+					// Send birthday message to channel
+				}
 			}
 			
-			if (now.getHours() == 19 && now.getMinutes() == 45) this.counter = 0;
+			if (now.getHours() == 10 && now.getMinutes() == 10) this.counter = 0;
 			
 			
 		}, 10000);
