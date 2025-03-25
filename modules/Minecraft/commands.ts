f9db248fa235c54ab32296b6cdc2aabc1b2300fa
AeroBytes <kelvin@aerobytes.nl>
2025-03-25 08:39:29 +0100

    Added Minecraft whitelist command

--- modules/Minecraft/commands.ts
+++ modules/Minecraft/commands.ts
@@ -3,5 +3,18 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 export const commands = [
-
-];
+	new SlashCommandBuilder()
+		.setName('minecraft')
+		.setDescription('Minecraft functies!')
+		.addSubcommand(add =>
+			add
+			.setName('whitelist')
+			.setDescription('Krijg toegang tot onze Minecraft servers!')
+			.addStringOption(option =>
+				option
+					.setName('gebruikersnaam')
+					.setDescription('Gebruikers naam van Minecraft.')
+					.setRequired(true)
+			)
+		)
+].map(commands => commands.toJSON());
