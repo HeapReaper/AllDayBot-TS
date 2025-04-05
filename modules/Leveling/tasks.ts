9fb3b357a5b36d260eead5c904cfe60ffa7de602
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-05 22:24:37 +0200

    Fully working message + voice XP

--- modules/Leveling/tasks.ts
+++ modules/Leveling/tasks.ts
@@ -4,36 +4,27 @@ import { Logging } from '@helpers/logging.ts';
 import LevelingEvents from './events.ts';
 import Database from '@helpers/database';
 import { Client, ChannelType, VoiceChannel, Guild } from 'discord.js';
+import {getEnv} from "@helpers/env.ts";
 
 export default class LevelingTasks {
     private client: Client;
+    private static taskRunning: boolean = false;
 
     // @ts-ignore
     constructor(client: Client) {
         this.client = client;
-        setInterval(async () => { void this.addXpToMembersTask(); }, 10000);
+        setInterval(async () => {
+            void this.addXpToMembersTask();
+        }, 60000);
     }
 
     async addXpToMembersTask(): Promise<void> {
         Logging.debug(`Running task addXpToMembersTask()`);
+
         // Messaging
         for (const userId of LevelingEvents.getUserXpAddedFromMessages()) {
             try {
-                const result: any = await Database.select('leveling', ['xp', 'level'], {user_id: userId});
-
-                if (result.length == 0) {
-                    await Database.insert('leveling', {'user_id': userId, 'xp': 15});
-                    return;
-                }
-
-                const newXp: number = result[0].xp + 15;
-
-                if (newXp < Math.floor(8.196 * Math.pow(result[0].level + 1, 2.65) + 200)) {
-                    await Database.update('leveling', {xp: newXp}, {user_id: userId});
-                    continue;
-                }
-
-                await Database.update('leveling', {xp: newXp, level: result[0].level + 1}, {user_id: userId});
+                await this.addXpToMember(userId, this.generateRandomNumber(15, 25));
             } catch (error: any) {
                 console.error(`Error processing user ${userId} in Leveling tasks: `, error);
             }
@@ -43,12 +34,10 @@ export default class LevelingTasks {
 
         // Voice
         try {
-            const guilds = await this.client.guilds.fetch();
-            const guild = guilds.first() as Guild;
+            Logging.debug('Looking into voice channels to add XP');
+            const guild = await this.client.guilds.fetch('1093873145313767495') as Guild;
 
             if (!guild) return;
-
-            console.log(`Checking voice channels in ${guild.name}`);
 
             // Fetch channels and filter voice channels
             const channels = await guild.channels.fetch();
@@ -57,10 +46,12 @@ export default class LevelingTasks {
             for (const voiceChannel of voiceChannels) {
                 if (!voiceChannel || !voiceChannel.members.size) continue;
 
-                console.log(`🔊 Checking voice channel: ${voiceChannel.name}`);
+                if (voiceChannel.name === 'AFK' || voiceChannel.name === 'afk') return;
 
                 for (const member of voiceChannel.members.values()) {
-                    console.log(`👤 Member in voice: ${member.user.tag}`);
+                    await this.addXpToMember(member.id, this.generateRandomNumber(4, 6));
+
+                    Logging.debug(`Someone is in VC: ${member.user.tag}`)
                 }
             }
         } catch (error) {
@@ -68,5 +59,28 @@ export default class LevelingTasks {
         }
 
         Logging.debug('Adding XP to members');
+
+    }
+
+    async addXpToMember(userId: String, xpToAdd: number) {
+        const result: any = await Database.select('leveling', ['xp', 'level'], {user_id: userId});
+        if (result.length == 0) {
+            await Database.insert('leveling', {'user_id': userId, 'xp': xpToAdd});
+            return;
+        }
+
+        const newXp: number = result[0].xp + xpToAdd;
+
+        if (newXp < Math.floor(8.196 * Math.pow(result[0].level + 1, 2.65) + 200)) {
+            await Database.update('leveling', {xp: newXp}, {user_id: userId});
+            return;
+        }
+
+        await Database.update('leveling', {xp: newXp, level: result[0].level + 1}, {user_id: userId});
+    }
+
+    // Returns a random number between min and max
+    generateRandomNumber(min: number, max: number): Number {
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 }
