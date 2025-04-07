37b8a359104c0957d2a431f328140d853db2c125
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-07 12:20:06 +0200

    Added level command listeners, closes #62

--- modules/Leveling/commandsListener.ts
+++ modules/Leveling/commandsListener.ts
@@ -17,56 +17,35 @@ export default class CommandsListener {
             if (!interaction.isCommand()) return;
 
             const { commandName } = interaction;
+            const subCommandName: any = interaction.options.getSubcommand();
 
-            switch (commandName) {
+            switch (subCommandName) {
                 case 'scorebord':
-                    await this.handleScoreBoardCommand(interaction);
+                    await this.handleScoreBoard(interaction);
                     break;
                 case 'level':
-                    await this.handleLevelCommand(interaction);
+                    await this.handleLevel(interaction);
+                    break;
+                case 'bereken_level':
+                    await this.handleCalculateLevel(interaction);
                     break;
             }
         });
     }
 
-    async handleScoreBoardCommand(interaction: Interaction): Promise<void> {
-        const canvasScorebord = new CanvasBuilder(250, 400);
-
-        await canvasScorebord.setBackground('https://img.freepik.com/free-vector/dark-hexagonal-background-with-gradient-color_79603-1409.jpg');
-        canvasScorebord.drawRect(0, 0, 250, 50, Color.Opacity50);
-        canvasScorebord.drawText('Scorebord', 22, 30, 'bold 24px Arial', Color.White)
-        Database.connect();
-        const LeaderBoardData = await Database.query('SELECT user_id, xp, level FROM leveling');
-        for (const [index, entry] of LeaderBoardData.entries()) {
-            try {
-                const user = await this.client.users.fetch(entry.user_id);
-                canvasScorebord.drawText(`${index + 1}.  ${user.displayName}`, 20, 80 + index * 33, 'bold 16px Arial', Color.White);
-                canvasScorebord.drawText(`Level: ${entry.level}, XP: ${entry.xp}`, 40, 80 + index * 33, 'bold 14px Arial', Color.White);
-            } catch (error: any) {
-                Logging.error(`Error fetching user: ${entry.user_id} - ${error.message}`);
-                canvasScorebord.drawText(`${index + 1}.  Onbekend`, 20, 80 + index * 33, 'bold 16px Arial', Color.White);
-                canvasScorebord.drawText(`Level: ${entry.level}, XP: ${entry.xp}`, 40, 80 + index * 33, 'bold 14px Arial', Color.White);
-            }
-        }
-
-        canvasScorebord.drawText('Pagina: 1', 20, 390, '10px Arial', Color.White);
-
-        const attachment = canvasScorebord.getBuffer();
-        // @ts-ignore
-        await interaction.reply({ files: [{ attachment, name: 'leaderboard.png' }] });
+    async handleScoreBoard(interaction: Interaction): Promise<void> {
+        //
     }
 
-    async handleLevelCommand(interaction: Interaction): Promise<void> {
-        //const userLevelData = await Database.query(`SELECT xp, level FROM leveling WHERE user_id = ${interaction.user.id}`);
+    async handleLevel(interaction: Interaction): Promise<void> {
+        //
+    }
 
-        const canvasLevel = new CanvasBuilder(400, 250);
-        await canvasLevel.setBackground('https://img.freepik.com/free-vector/dark-hexagonal-background-with-gradient-color_79603-1409.jpg');
+    async handleCalculateLevel(interaction: Interaction): Promise<void> {
 
-        canvasLevel.drawRect(0, 0, 400, 50, Color.Opacity50);
-        canvasLevel.drawText(`🔥 Level van ${interaction.user.displayName} 🔥`, 46, 33, 'bold 24px Arial', Color.White);
+    }
 
-        const attachment = canvasLevel.getBuffer();
-        // @ts-ignore
-        await interaction.reply({ files: [{ attachment, name: 'userLevel.png' }] });
+    createLevelEmbed() {
+
     }
 }
