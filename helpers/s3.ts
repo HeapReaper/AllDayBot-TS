59193c035505fda399d762951709e5213da2b5b5
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-18 12:30:25 +0200

    Working S3 helper

--- helpers/s3.ts
+++ helpers/s3.ts
@@ -2,35 +2,40 @@ import * as Minio from 'minio';
 import { getEnv } from '@helpers/env';
 import { Logging } from '@helpers/logging';
 
-class S3 {
-    static minioClient: any;
-    static bucketName: string;
+export default class S3OperationBuilder {
+    private static minioClient: any;
+    private bucketName: string | undefined;
 
     static init(): void {
-        // @ts-ignore
-        S3.minioClient = new Minio.Client({
+        if (S3OperationBuilder.minioClient) return;
+
+        S3OperationBuilder.minioClient = new Minio.Client({
             endPoint: <string>getEnv('S3_ENDPOINT'),
             port: <number>getEnv('S3_PORT'),
-            useSSL: <boolean>getEnv('S3_SSL'),
+            useSSL: false,
             accessKey: <string>getEnv('S3_ACCESS_KEY'),
             secretKey: <string>getEnv('S3_SECRET_KEY'),
         });
     }
 
-    static setBucket(bucketName: string): void {
-        S3.bucketName = bucketName;
+    static setBucket(bucketName: string): S3OperationBuilder {
+        S3OperationBuilder.init();
+
+        const builder = new S3OperationBuilder();
+        builder.bucketName = bucketName;
+
+        return builder;
     }
 
-    static async uploadFile(objectName: string, filePath: string): Promise<any> {
-        if (!S3.minioClient) S3.init();
-
-        if (!S3.bucketName) throw new Error('Bucket name is missing!');
-
+    async uploadFileFromPath(objectName: string, filePath: string): Promise<any> {
         try {
-            await S3.minioClient.fPutObject(S3.bucketName, objectName, filePath);
-            Logging.info('Uploaded file to S3');
+            await S3OperationBuilder.minioClient.fPutObject(this.bucketName, objectName, filePath);
+
+            Logging.debug('S3 upload successful!');
+            return { success: true };
         } catch (error) {
-            Logging.error(`Error inside S3 helper: ${error}`);
+            Logging.debug(`S3 upload failed: ${error}`);
+            return { success: false, error: error };
         }
     }
 }
