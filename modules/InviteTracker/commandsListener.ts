bd174e332efebb21dbdc4d5008fd940cd9d91643
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-25 22:18:19 +0200

    Refactor `addInvite` method to use QueryBuilder and Logging
    Update the `addInvite` method to utilize a database query builder for inserting invite data. Implement error handling and logging for better debugging and maintainability.

--- modules/InviteTracker/commandsListener.ts
+++ modules/InviteTracker/commandsListener.ts
@@ -4,6 +4,8 @@ import {
 	Events as discordEvents,
 	MessageFlags
 } from 'discord.js';
+import QueryBuilder from '@utils/database.ts';
+import { Logging } from '@utils/logging';
 
 export default class CommandsListener {
 	private client: Client;
@@ -40,11 +42,29 @@ export default class CommandsListener {
 		//
 	}
 
-	async addInvite(interaction: Interaction) {
-		//
+	async addInvite(interaction: Interaction): Promise<void> {
+		try {
+			await QueryBuilder
+				.insert('invite_tracker')
+				.values({
+					invite_name: interaction.options.getString('naam'),
+					inviter_id: `${interaction.options.getUser('invite_eigenaar').id}`,
+					invite_code: interaction.options.getString('invite_code'),
+					added_by_user_id: interaction.user.id
+				})
+				.execute();
+
+			// @ts-ignore
+			await interaction.reply({
+				content: 'Is toegevoegd! Gebruik `/invites lijst` om ze te zien.',
+				ephemeral: true,
+			});
+		} catch (error) {
+			Logging.error(`Error adding invite tracker: ${error}`);
+		}
 	}
 
-	async deleteInvite(interaction: Interaction) {
+	async deleteInvite(interaction: Interaction): Promise<void> {
 		//
 	}
 }
