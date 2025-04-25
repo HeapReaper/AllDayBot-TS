c0e4e976de6eed05f8a5f46ddac897992ab19833
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-25 10:17:21 +0200

    Refactor community command structure
    - Renamed `vraag_om_te_vragen` to `community`
    - Added subcommands for different actions within the community module
      - `vraag`: Send a non-ask question embed
      - `dm`: Send a DM request embed
      - `moeilijk_doen`: Mark as not too difficult embed
      - `kanaal`: Incorrect channel embed

--- modules/Community/commands.ts
+++ modules/Community/commands.ts
@@ -2,5 +2,50 @@ import { SlashCommandBuilder } from 'discord.js';
 
 export const commands = [
     new SlashCommandBuilder()
-        .setName('vraag_om_te_vragen')
+        .setName('community')
+        .setDescription('Alle community commands')
+        .addSubcommand(add =>
+            add
+            .setName('vraag')
+            .setDescription('Stuur een vraag niet om te vragen embed')
+            .addMentionableOption(option =>
+                option
+                .setName('gebruiker')
+                .setDescription('Selecteer de gebruiker')
+                .setRequired(true)
+            )
+        )
+        .addSubcommand(add =>
+            add
+            .setName('dm')
+            .setDescription('Stuur een dat hoeft niet in DM embed')
+            .addMentionableOption(option =>
+                option
+                .setName('gebruiker')
+                .setDescription('Selecteer de gebruiker')
+                .setRequired(true)
+            )
+        )
+        .addSubcommand(add =>
+            add
+            .setName('moeilijk_doen')
+            .setDescription('Stuur een doe niet zo moeilijk embed')
+            .addMentionableOption(option =>
+                option
+                .setName('gebruiker')
+                .setDescription('Selecteer de gebruiker')
+                .setRequired(true)
+            )
+        )
+        .addSubcommand(add =>
+            add
+            .setName('kanaal')
+            .setDescription('Stuur een verkeerd kanaal embed')
+            .addMentionableOption(option =>
+                option
+                .setName('gebruiker')
+                .setDescription('Selecteer de gebruiker')
+                .setRequired(true)
+            )
+        )
 ].map(commands => commands.toJSON());
