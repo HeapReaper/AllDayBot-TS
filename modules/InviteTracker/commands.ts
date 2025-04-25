dcc8693923a96b1e56250125f029e63000925876
AeroBytesNL <kelvin@aerobytes.nl>
2025-04-25 21:52:42 +0200

    Add detailed invite tracking commands
    - Added `invites` slash command with subcommands for listing, adding, and deleting entries.
    - Each subcommand includes specific options for required data (e.g., name, owner, code).
    - Updated the export to map the commands to JSON format.

--- modules/InviteTracker/commands.ts
+++ modules/InviteTracker/commands.ts
@@ -1,5 +1,58 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 export const commands = [
-
-].map(commands => commands.toJSON());
+    new SlashCommandBuilder()
+        .setName('invites')
+        .setDescription('De invite tracker')
+        // List
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('lijst')
+                .setDescription('Lijst van de invite trackers')
+        )
+        // Add
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('toevoegen')
+                .setDescription('Voeg een invite toe aan de tracker')
+                .addStringOption(option =>
+                    option
+                        .setName('naam')
+                        .setDescription('De naam van de invite tracker')
+                        .setRequired(true)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('invite_eigenaar')
+                        .setDescription('De eigenaar van de invite link')
+                        .setRequired(true)
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('invite_code')
+                        .setDescription('Invite code')
+                        .setRequired(true)
+                )
+        )
+        // Delete
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('verwijder')
+                .setDescription('Verwijder een entry van de invite tracker per id, invite_code of invite naam')
+                .addIntegerOption(option =>
+                    option
+                        .setName('id')
+                        .setDescription('Invite ID')
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('code')
+                        .setDescription('Invite code (url)')
+                )
+                .addStringOption(option =>
+                    option
+                        .setName('invite_naam')
+                        .setDescription('Invite naam')
+                )
+        ),
+].map(command => command.toJSON());
