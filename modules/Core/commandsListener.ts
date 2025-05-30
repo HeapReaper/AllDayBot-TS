31eb0e851ba2730762fab86ef63368116787c5f7
AeroBytesNL <kelvin@aerobytes.nl>
2025-05-30 20:33:47 +0200

    Basic core status command listener

--- modules/Core/commandsListener.ts
+++ modules/Core/commandsListener.ts
@@ -1,4 +1,9 @@
-import { Client, Interaction, Events, MessageFlags} from 'discord.js';
+import {
+    Client,
+    Interaction,
+    Events as discordEvents,
+    ChatInputCommandInteraction
+} from 'discord.js';
 
 export default class CommandsListener {
 	private client: Client;
@@ -8,7 +13,27 @@ export default class CommandsListener {
 		void this.commandsListener();
 	}
 	
-	async commandsListener(): Promise<void> {
-		//
-	}
+    async commandsListener(): Promise<void> {
+        this.client.on(discordEvents.InteractionCreate, async (interaction: Interaction): Promise<void> => {
+            if (!interaction.isCommand()) return;
+
+            const { commandName } = interaction;
+
+            if (commandName !== 'core') return;
+
+            // @ts-ignore
+            const subCommandName: any = interaction.options.getSubcommand();
+
+            switch (subCommandName) {
+                case 'status':
+                    // @ts-ignore
+                    await this.handleStatus(interaction);
+                    break;
+            }
+        });
+    }
+
+    async handleStatus(interaction: ChatInputCommandInteraction): Promise<void> {
+        await interaction.reply('Working on it!');
+    }
 }
