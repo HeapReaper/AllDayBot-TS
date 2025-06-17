123a46bf8195d1175436deb9f3017e08c1a6b7ab
AeroBytesNL <kelvin@aerobytes.nl>
2025-06-17 09:59:39 +0200

    Added comments Minecraft

--- modules/Minecraft/commands.ts
+++ modules/Minecraft/commands.ts
@@ -1,28 +1,50 @@
 import { SlashCommandBuilder } from 'discord.js';
 
-export const commands = [
-	new SlashCommandBuilder()
-		.setName('minecraft')
-		.setDescription('Minecraft functies!')
-		.addSubcommand(add =>
-			add
-			.setName('whitelist')
-			.setDescription('Krijg toegang tot onze Minecraft servers!')
-			.addStringOption(option =>
-				option
-				.setName('gebruikersnaam')
-				.setDescription('Gebruikers naam van Minecraft.')
-				.setRequired(true)
-			)
-		)
-		.addSubcommand(add =>
-			add
-			.setName('verwijder_whitelist')
-			.setDescription('Verwijder je whitelist van onze bot.')
-		)
-		.addSubcommand(add =>
-			add
-			.setName('online')
-			.setDescription('Zie wie online is!')
-		)
-].map(commands => commands.toJSON());
+/**
+ * Subcommand: Add yourself to the Minecraft whitelist
+ * @param {string} gebruikersnaam - Your Minecraft username
+ * @example
+ * /minecraft whitelist gebruikersnaam:Notch
+ */
+export const whitelistCommand = new SlashCommandBuilder()
+	.setName('whitelist')
+	.setDescription('Krijg toegang tot onze Minecraft servers!')
+	.addStringOption(option =>
+		option
+			.setName('gebruikersnaam')
+			.setDescription('Gebruikers naam van Minecraft.')
+			.setRequired(true)
+	);
+
+/**
+ * Subcommand: Remove yourself for the whitelist
+ * @example
+ * /minecraft verwijder_whitelist
+ */
+export const verwijderWhitelistCommand = new SlashCommandBuilder()
+	.setName('verwijder_whitelist')
+	.setDescription('Verwijder je whitelist van onze bot.');
+
+/**
+ * Subcommand: See who's online on the Minecraft servers
+ * @example
+ * /minecraft online
+ */
+export const onlineCommand = new SlashCommandBuilder()
+	.setName('online')
+	.setDescription('Zie wie online is!');
+
+export const minecraftCommand = new SlashCommandBuilder()
+	.setName('minecraft')
+	.setDescription('Minecraft functies!')
+	.addSubcommand(sub =>
+		sub.setName('whitelist').setDescription(whitelistCommand.description!)
+	)
+	.addSubcommand(sub =>
+		sub.setName('verwijder_whitelist').setDescription(verwijderWhitelistCommand.description!)
+	)
+	.addSubcommand(sub =>
+		sub.setName('online').setDescription(onlineCommand.description!)
+	);
+
+export const commands = [minecraftCommand.toJSON()];
